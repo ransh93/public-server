a51250c593bec449c831d9b705c40bbcbeee806d
Ran shister <ranshister@Rans-MacBook-Air.local>
2022-12-09 15:13:00 +0200

    change response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Yo Shister!')
 })
 
 app.listen(process.env.PORT || 3000)
